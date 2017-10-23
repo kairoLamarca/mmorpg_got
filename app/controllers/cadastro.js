6a9f2539f8910f242428154757a20c85e7c4579d
kairolamarca <lamarca.kairo@gmail.com>
2017-10-23 21:58:02 -0200

    cadastro route, controller e view
    rota de post com metodo cadastrar no controle, validando os campos e passando para a view

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,21 @@
 module.exports.cadastro = function(application, req, res){
-    res.render('cadastro');
+    res.render('cadastro', {validacao: {}, dadosForm: {}});
+}
+
+module.exports.cadastrar = function(application, req, res){
+    let dadosForm = req.body;
+
+    req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+    req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha não pode ser vazio').notEmpty();
+    req.assert('casa', 'Casa não pode ser vazio').notEmpty();
+
+    let erros = req.validationErrors();
+
+    if(erros){
+        res.render('cadastro', {validacao: erros, dadosForm: dadosForm});
+        return;
+    }
+
+    res.send('podemos cadastrar');
 }
