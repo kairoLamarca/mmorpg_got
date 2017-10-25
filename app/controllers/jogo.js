f15d1fee13061c10fd68ca105b54391e6115a64e
kairolamarca <lamarca.kairo@gmail.com>
2017-10-24 23:22:41 -0200

    destroy session
    implementação do botão sair, destruindo a sessão e voltando pra tela de login

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,10 +1,18 @@
-module.exports.jogo = function(application, req, res){
+module.exports.jogo = function (application, req, res) {
 
-    if(req.session.autorizado){
+    if (req.session.autorizado) {
         res.render('jogo');
     }
-    else{
-        res.render('index', {validacao: [{msg:'É necessário efetuar o login primeiro!'}]});
+    else {
+        res.render('index', { validacao: [{ msg: 'É necessário efetuar o login primeiro!' }] });
     }
-    
+
+}
+
+module.exports.sair = function (application, req, res) {
+
+    req.session.destroy((err) => {//apaga toda a session
+        res.render('index', {validacao: {}});
+    });
+
 }
