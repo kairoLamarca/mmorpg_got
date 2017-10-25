38388183dbf9eaf10634876854c766fa7a0b2bfe
kairolamarca <lamarca.kairo@gmail.com>
2017-10-24 22:19:48 -0200

    autenticacao
    criado metodo de autenticacaoe adicionado dependencia do express-session

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,19 @@
-module.exports.index = function(application, req, res){
-    res.render('index');
+module.exports.index = function (application, req, res) {
+    res.render('index', {validacao: {}});
+}
+
+module.exports.autenticar = function (application, req, res) {
+    let dadosForm = req.body;
+
+    req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha não pode ser vazio').notEmpty();
+
+    let erros = req.validationErrors();
+
+    if (erros) {
+        res.render('index', { validacao: erros });
+        return;
+    }
+
+    res.send('ok');
 }
