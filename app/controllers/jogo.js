af40e7d2232396c99ad630fb27ececd98cdbb724
kairolamarca <lamarca.kairo@gmail.com>
2017-10-24 23:11:11 -0200

    validação de acesso
    validação de acesso da página jogo de acordo com a session de autenticação

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,3 +1,10 @@
 module.exports.jogo = function(application, req, res){
-    res.render('jogo');
+
+    if(req.session.autorizado){
+        res.render('jogo');
+    }
+    else{
+        res.render('index', {validacao: [{msg:'É necessário efetuar o login primeiro!'}]});
+    }
+    
 }
