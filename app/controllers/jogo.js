2a7d06c5d3acbad63588031938fe5b4400f969b2
kairolamarca <lamarca.kairo@gmail.com>
2017-10-25 23:16:25 -0200

    suditos
    validação da view sudito com inclusã dela dentro da tela do jogo e msg de erro se não informar os campos

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -5,13 +5,18 @@ module.exports.jogo = function (application, req, res) {
         return;
     }
 
+    let comando_invalido = 'N';
+    if(req.query.comando_invalido == 'S'){
+        comando_invalido = 'S';
+    }
+
     let usuario = req.session.usuario;
     let casa = req.session.casa;
 
     const connection = application.config.dbConnection;
     const JogoDAO = new application.app.models.JogoDAO(connection);
 
-    JogoDAO.iniciaJogo(res, usuario, casa);
+    JogoDAO.iniciaJogo(res, usuario, casa, comando_invalido);
 
     
 }
@@ -23,9 +28,40 @@ module.exports.sair = function (application, req, res) {
 }
 
 module.exports.suditos = function (application, req, res) {
+    if (req.session.autorizado !== true) {
+        res.render('index', { validacao: [{ msg: 'É necessário efetuar o login primeiro!' }] });
+        return;
+    }
+
     res.render('aldeoes');
 }
 
 module.exports.pergaminhos = function (application, req, res) {
+    if (req.session.autorizado !== true) {
+        res.render('index', { validacao: [{ msg: 'É necessário efetuar o login primeiro!' }] });
+        return;
+    }
+
     res.render('pergaminhos');
+}
+
+module.exports.ordenar_acao_sudito = function (application, req, res) {
+    if (req.session.autorizado !== true) {
+        res.render('index', { validacao: [{ msg: 'É necessário efetuar o login primeiro!' }] });
+        return;
+    }
+    
+    let dadosForm = req.body;
+
+    req.assert('acao', 'Ação deve ser informada').notEmpty();
+    req.assert('quantidade', 'Quantidade deve ser informada').notEmpty();
+
+    let erros = req.validationErrors();
+
+    if(erros){
+        res.redirect('jogo?comando_invalido=S');
+        return;
+    }
+
+    res.send('ok');
 }
